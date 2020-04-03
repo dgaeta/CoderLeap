acfbb6070707f611b38f87e8cea90b6dfacf63e3
dangaeta <gaeta.d@gmail.com>
2020-04-03 08:18:27 -0500

    Initial Commit

---
+++ CoderLeap/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoderLeap
+//
+//  Created by Daniel Gaeta on 4/3/20.
+//  Copyright © 2020 CoderLeap, Corp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
